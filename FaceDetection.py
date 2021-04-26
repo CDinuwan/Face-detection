e8f2cae0ef3a53c3cae13fd5875a1c4888f6317a
CDinuwan <chanukadinuwan35@gmail.com>
2021-04-26 15:51:44 +0530

    Face detection

--- FaceDetection.py
+++ FaceDetection.py
@@ -1,14 +1,14 @@
 import cv2
 
 face_cascade = cv2.CascadeClassifier("C:\\Users\\Chanuka Dinuwan\\Desktop\\Face Detection\\haarcascade_frontalface_default.xml")
-img = cv2.imread("C:\\Users\\Chanuka Dinuwan\\Desktop\\Face Detection\\img.jpg")
+img = cv2.imread("C:\\Users\\Chanuka Dinuwan\\Desktop\\Face Detection\\img.png")
 
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.05, minNeighbors=5)
 
 for x,y,z in faces:
-    img=cv2.rectangle(img,(x,y),(0,255,0),3)
+    img=cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
 
 print(type(faces))
 print(faces)
